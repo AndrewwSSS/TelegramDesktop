c489afe82392f9b2f6bf0acf17bc58de9f93fe5b
ExistedGit <paytvemail@gmail.com>
2022-05-12 15:48:05 +0300

    CommonLibrary

---
+++ CommonLibrary/GroupChat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace CommonLibrary
+{
+    public enum GroupType
+    {
+        Personal,
+        Public
+    }
+
+    public class GroupChat
+    {
+        public string Guid { get; private set; } = System.Guid.NewGuid().ToString();
+        public GroupType Type { get; set; }
+        public List<User> Members { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<ImageSource> Images { get; set; }
+        public ImageSource Avatar => Images == null && Images.Count == 0 ? null : Images[0];
+        public List<ChatMessage> Messages { get; set; }
+    }
+}
--- MessageLibrary/TextMessage.cs
+++ MessageLibrary/TextMessage.cs
@@ -12,14 +12,11 @@ namespace MessageLibrary
     public class TextMessage : Message
     {
         public string Text { get; set; }
-        public bool DisplayTime = false;
         public TextMessage(string text)
         {
             Type = MessageType.Text;
-            Time = DateTime.Now;
             Text = text;
         }
-        public override string ToString() => $"{(DisplayTime ? "[" + Time.ToLongTimeString() + " " + Time.ToShortDateString() + "] " : "")}{Text}";
-        
+        public override string ToString() => Text;
     }
 }
