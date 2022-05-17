a63ec25776f253f62d95651da6091be0803f48b4
ExistedGit <paytvemail@gmail.com>
2022-05-17 20:15:53 +0300

    INotifyPropertyChanged

--- CommonLibrary/Messages/Auth/Groups/GroupChat.cs
+++ CommonLibrary/Messages/Auth/Groups/GroupChat.cs
@@ -1,8 +1,11 @@
 ﻿using CommonLibrary.Messages.Users;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Windows.Media;
 
 namespace CommonLibrary.Messages.Groups
@@ -23,7 +26,11 @@ namespace CommonLibrary.Messages.Groups
         public GroupType Type { get; set; }
         public DateTime DateCreated { get; set; }
         public List<ChatMessage> Messages { get; set; }
+        public ChatMessage LastMessage => Messages.LastOrDefault();
         public List<User> Members { get; set; }
+        [field: NonSerialized]
+        public event PropertyChangedEventHandler PropertyChanged;
 
+        public void OnPropertyChanged([CallerMemberName] string propertyName = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
