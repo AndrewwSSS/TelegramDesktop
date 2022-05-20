686d9584c17a5a352ad92a55dc6e3c221065804e
DedFama <clash2clans1one@gmail.com>
2022-05-20 19:14:48 +0200

    Server : add treatment GroupJoinMessage

---
+++ CommonLibrary/Messages/Auth/Groups/GroupInviteMessage.cs
@@ -0,0 +1,19 @@
+﻿using CommonLibrary.Messages.Groups;
+using System;
+
+namespace CommonLibrary.Messages.Auth.Groups
+{
+    [Serializable]
+    public class GroupInviteMessage : BaseMessage
+    {
+        public PublicGroupInfo GroupInfo { get; set; }
+        public int FromUserId { get; set; }
+
+        public GroupInviteMessage(PublicGroupInfo GroupInfo, int FromUserId)
+        {
+            this.GroupInfo = GroupInfo;
+            this.FromUserId = FromUserId;
+        }
+
+    }
+}
--- CommonLibrary/Messages/Auth/Groups/GroupJoinResultMessage.cs
+++ CommonLibrary/Messages/Auth/Groups/GroupJoinResultMessage.cs
@@ -1,13 +1,14 @@
 ﻿using System;
+using CommonLibrary.Messages.Groups;
 using MessageLibrary;
 
 namespace CommonLibrary.Messages.Auth.Groups
 {
     [Serializable]
-    public class GroupJoinResultMessage : Message
+    public class GroupJoinResultMessage : BaseMessage
     {
         public AuthenticationResult Result { get; set; }
-
+    
         public GroupJoinResultMessage(AuthenticationResult Result) => this.Result = Result;
 
         public GroupJoinResultMessage() { }
