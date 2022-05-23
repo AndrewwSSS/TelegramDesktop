1cc8bef67a9d4398b2d87ad18d20a5b496acaa3f
ExistedGit <paytvemail@gmail.com>
2022-05-23 16:43:42 +0300

    + GroupUpdateMessage

---
+++ CommonLibrary/Messages/Groups/GroupUpdateMessage.cs
@@ -0,0 +1,22 @@
+﻿using CommonLibrary.Messages.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLibrary.Messages.Groups
+{
+    [Serializable]
+    public class GroupUpdateMessage : BaseMessage
+    {
+        public List<PublicUserInfo> NewUsers { get; set; }
+        public List<PublicUserInfo> RemovedUsers { get; set; }
+        public string NewDescription { get; set; }
+        public string NewName { get; set; }
+        public GroupUpdateMessage()
+        {
+
+        }
+    }
+}
