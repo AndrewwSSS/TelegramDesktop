ed877dccc947760806f1e3744942d9186b62c813
ExistedGit <paytvemail@gmail.com>
2022-05-26 19:05:31 +0300

    во славу Плети

--- CommonLibrary/Containers/ImageContainer.cs
+++ CommonLibrary/Containers/ImageContainer.cs
@@ -56,6 +56,10 @@ namespace CommonLibrary.Containers
         }
 
 
+        public ImageContainer()
+        {
+
+        }
         public ImageContainer(byte[] data)
         {
             this.Data = data;
@@ -81,7 +85,7 @@ namespace CommonLibrary.Containers
                 throw new ArgumentException($"Не удалось создать ImageContainer: изображение {path} не существует");
             if (!AllowedExtensions.Contains(new FileInfo(path).Extension))
                 throw new ArgumentException($"Не удалось создать ImageContainer: формат изображения {path} не поддерживается");
-            ImageContainer result = new ImageContainer
+            ImageContainer result = new ImageContainer()
             {
                 Name = Path.GetFileName(path),
                 Data = File.ReadAllBytes(path)
