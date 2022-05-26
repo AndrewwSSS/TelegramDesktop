99416bb895b93a4faee399edb47d5ccc2c548acc
DedFama <clash2clans1one@gmail.com>
2022-05-26 16:24:17 +0200

    CreateGroupMessage : Image to byte[]

--- CommonLibrary/Messages/Groups/CreateGroupMessage.cs
+++ CommonLibrary/Messages/Groups/CreateGroupMessage.cs
@@ -1,7 +1,6 @@
-﻿using System;
+﻿using MessageLibrary;
+using System;
 using System.Collections.Generic;
-using CommonLibrary.Containers;
-using MessageLibrary;
 
 namespace CommonLibrary.Messages.Groups
 {
@@ -9,12 +8,12 @@ namespace CommonLibrary.Messages.Groups
     public class CreateGroupMessage : Message
     {
         public string Name { get; set; }
-        public ImageContainer Image { get; set; }
+        public byte[] Image { get; set; }
         public List<int> MembersId { get; set; } 
         public int FromUserId { get; set; }
 
 
-        public CreateGroupMessage(string Name, int FromUserId, List<int> MembersId = null, ImageContainer Image = null) : this()
+        public CreateGroupMessage(string Name, int FromUserId, List<int> MembersId = null, byte[] Image = null) : this()
         {
             this.Name = Name;
             this.Image = Image;
