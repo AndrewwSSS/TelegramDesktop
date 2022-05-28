d5252579aca2743c5a4b227afd5d8b0e86f3bf92
DedFama <clash2clans1one@gmail.com>
2022-05-28 14:24:13 +0200

    Add FastLoginMessages

---
+++ CommonLibrary/Messages/Auth/Login/FastLoginMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLibrary.Messages.Auth.Login
+{
+    internal class FastLoginMessage
+    {
+    }
+}
