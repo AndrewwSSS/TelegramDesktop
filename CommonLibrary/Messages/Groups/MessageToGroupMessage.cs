e084e4228ba005f33208a71fccf1e7b2d21631ba
DedFama <clash2clans1one@gmail.com>
2022-06-02 16:00:26 +0200

    Add: MessageToGroupMessage, ChatMessageSendResult

---
+++ CommonLibrary/Messages/Groups/MessageToGroupMessage.cs
@@ -0,0 +1,20 @@
+﻿using MessageLibrary;
+using System;
+
+
+namespace CommonLibrary.Messages.Groups
+{
+    [Serializable]
+    public class MessageToGroupMessage : Message
+    {
+        public int LocalMessageId { get; set; }
+        public ChatMessage Message { get; set; }
+
+
+        public MessageToGroupMessage(ChatMessage message, int localId)
+        {
+            LocalMessageId = localId;
+            Message = message;
+        }
+    }
+}
