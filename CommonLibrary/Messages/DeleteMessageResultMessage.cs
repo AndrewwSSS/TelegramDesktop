2975f089ee4c1b52a90adeee29aed557d3e9c60a
DedFama <clash2clans1one@gmail.com>
2022-06-02 21:24:14 +0200

    add DeleteMessageResultMessage

---
+++ CommonLibrary/Messages/DeleteMessageResultMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonLibrary.Messages.Auth;
+
+namespace CommonLibrary.Messages
+{
+    [Serializable]
+    public class DeleteMessageResultMessage : BaseMessage
+    {
+        public AuthenticationResult Result { get; set; }
+
+        public DeleteMessageResultMessage(AuthenticationResult result) => Result = result;
+
+        public DeleteMessageResultMessage() { }
+    }
+}
