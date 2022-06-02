b7a18ee660f53121bf6b4cbff0e21e45f695fa27
DedFama <clash2clans1one@gmail.com>
2022-06-02 15:44:08 +0200

    Add DeleteMessage

---
+++ CommonLibrary/Messages/DeleteMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CommonLibrary.Messages
+{
+    [Serializable]
+    public class DeleteMessage : BaseMessage
+    {
+        public int MessageId { get; set; }
+        public int GroupId { get; set; }
+        public int FromUserId { get; set; }
+
+        public DeleteMessage(int messageId, int groupId, int fromUserId)
+        {
+            MessageId = messageId;
+            GroupId = groupId;
+            FromUserId = fromUserId;
+        }
+
+        public DeleteMessage() { }
+    }
+}
