6b6067ed902873a547f02b50336c28f7a3fa536d
DedFama <clash2clans1one@gmail.com>
2022-06-15 12:13:01 +0200

    Add MetadataResultMessage

---
+++ CommonLibrary/Messages/Files/MetadataResultMessage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLibrary.Messages.Files
+{
+    public class MetadataResultMessage
+    {
+        //LocalId - DbId
+        public List<KeyValuePair<int, int>> Images { get; set; }
+        public List<KeyValuePair<int, int>> Files { get; set; }
+
+
+        public MetadataResultMessage()
+        {
+            Images = new List<KeyValuePair<int, int>>();
+            Files = new List<KeyValuePair<int, int>>();
+        }
+
+    }
+}
