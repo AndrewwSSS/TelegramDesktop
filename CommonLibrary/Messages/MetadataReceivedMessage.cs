69c283870c15d03ed1cdc6044306d5536ace9f2d
DedFama <clash2clans1one@gmail.com>
2022-06-16 19:49:19 +0200

    merge

--- CommonLibrary/Messages/MetadataReceivedMessage.cs
+++ CommonLibrary/Messages/MetadataReceivedMessage.cs
@@ -1,9 +1,5 @@
 ﻿using MessageLibrary;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CommonLibrary.Messages
 {
