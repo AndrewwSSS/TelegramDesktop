9b8f992078a7d705c8872d4ae6b97a7f7fa67d9e
ExistedGit <paytvemail@gmail.com>
2022-06-16 19:26:37 +0200

    Metadata sync

--- CommonLibrary/Messages/Files/MetadataMessage.cs
+++ CommonLibrary/Messages/Files/MetadataMessage.cs
@@ -12,10 +12,11 @@ namespace CommonLibrary.Messages.Files
         // Списки пар LocalID - Image/File metadata
         public List<KeyValuePair<int, ImageMetadata>> Images { get; set; }
         public List<KeyValuePair<int, FileMetadata>> Files { get; set; }
-
         public int LocalMessageId { get; set; }
-        public MetadataMessage(int msgId, IEnumerable<KeyValuePair<int, ImageMetadata>> images = null, IEnumerable<KeyValuePair<int, FileMetadata>> files = null)
+        public int LocalReturnId { get; set; }
+        public MetadataMessage(int msgId, int lrId, IEnumerable<KeyValuePair<int, ImageMetadata>> images = null, IEnumerable<KeyValuePair<int, FileMetadata>> files = null)
         {
+            LocalReturnId = lrId;
             LocalMessageId = msgId;
             Images = images?.ToList();
             Files = files?.ToList();
