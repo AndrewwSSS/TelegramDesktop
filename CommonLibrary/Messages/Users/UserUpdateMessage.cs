459f8c0722e989b18c5c121cf3afd50cd502de6e
DedFama <clash2clans1one@gmail.com>
2022-07-13 17:49:44 +0200

    fix

--- CommonLibrary/Messages/Users/UserUpdateMessage.cs
+++ CommonLibrary/Messages/Users/UserUpdateMessage.cs
@@ -4,10 +4,10 @@ using System;
 namespace CommonLibrary.Messages.Users
 {
     [Serializable]
-    public class UserUpdateMessage : Message
+    public class UserUpdateMessage : BaseMessage
     {
         public int UserId { get; set; }
-        public bool OnlineStatus { get; set; }
+        public bool? OnlineStatus { get; set; }
         public string NewDescription { get; set; }
         public string NewName { get; set; }
         public string NewLogin { get; set; }
