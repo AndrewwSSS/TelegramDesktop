14a473560b1ff25f40ab5666a813446d90b959b2
ExistedGit <66470490+ExistedGit@users.noreply.github.com>
2022-08-07 14:01:43 +0200

    usergroup caching fix

--- Telegram/View/MainWindow.Caching.cs
+++ Telegram/View/MainWindow.Caching.cs
@@ -19,20 +19,29 @@ namespace Telegram
         public ObservableCollection<GroupItemWrap> FoundGroups { get; set; } = new ObservableCollection<GroupItemWrap>();
 
         public Dictionary<int, string> CachedImages { get; set; } = new Dictionary<int, string>();
-        
+
         private void LoadUsers() => CachedUsers.AddRange(CacheManager.Instance.LoadAllUsers());
-        private void LoadGroups() {
+        private void LoadGroups()
+        {
             List<CacheManager.CachedGroupInfo> list = CacheManager.Instance.LoadAllGroups();
-            foreach(var info in list)
+            foreach (var info in list)
             {
                 GroupItemWrap item = new GroupItemWrap(info);
-                foreach(var id in info.MembersId)
+                foreach (var id in info.MembersId)
                 {
                     UserItemWrap user = CachedUsers.FirstOrDefault(u => u.User.Id == id);
                     if (user != null)
                         item.Members.Add(user);
                 }
                 item.AssociatedUserId = info.AssociatedUserId;
+                var associatedUser = CachedUsers.FirstOrDefault(u => u.User.Id == item.AssociatedUserId);
+                if (associatedUser != null)
+                {
+                    if (item.Name == null)
+                        item.GroupChat.Name = associatedUser.User.Name;
+                    if (item.Description == null)
+                        item.GroupChat.Description = associatedUser.User.Description;
+                }
                 Groups.Add(item);
                 CachedGroups.Add(item);
             }
